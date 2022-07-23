e3de7acd079c8b37b0781d54e307f2c11e9ec91b
Angel Tyler <102939438+angeltyler01@users.noreply.github.com>
2022-07-22 20:30:13 -0700

    finished script to use x and o in square spaces on board.

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
-//changed the var to const because the variable is not going to change.
-const square = document.querySelectorAll('.square');
+//changed currentPlayer to let instead of const because the variable does change.
+const squares = document.querySelectorAll('.square');
 const playerX = 'x'
 const playerO = 'o'
-const currentPlayer = playerX
+let currentPlayer = playerX
 
-const gameState = Array(square.length)
-gameState.fill(null)
+//Make an empty array for the squares inside the board. I used an array instead of the dataset attriute because my console kept sending errors.
+const gameState = ['null','null','null','null','null','null','null','null','null']
+
+
+//click sound
+const click = new Audio('/assets/click.mp3');
+
+//
+squares.forEach((square) => square.addEventListener('click', clickSquare))
+
+//which box was clicked
+function clickSquare(event) {
+    
+    const square = event.target
+    
+    if(square.innerText !=''){
+        return;
+    }
+    if(currentPlayer === playerX){
+        square.innerText = playerX
+        gameState[gameState-1] = playerX
+        currentPlayer= playerO
+    }
+    else{
+        square.innerText = playerO
+        gameState[gameState-1] = playerO
+        currentPlayer = playerX
+    }
+    click.play()
+    winner()
+}
+const playerWins= [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+function winner(){
+    for( const playerWin of playerWins){}
+}
+
+  
