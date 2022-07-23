40defc21f909360b08ebc23631aab729aea2dd17
Angel Tyler <102939438+angeltyler01@users.noreply.github.com>
2022-07-23 08:33:36 -0700

    Tried to fix JS code again and added to README.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TicTacToe-Project
+Get three x's or o's in  a row to win. 
+2 player game.
+
+Tutorials credit:
+https://www.youtube.com/watch?v=fPew9OI2PnA
+https://www.youtube.com/watch?v=AzvpHNkjqsg&t=786s
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let currentPlayer = playerX
 let gameOver= false
 
 //Make an empty array for the squares inside the board. I used an array instead of the dataset attriute because my console kept sending errors.
-const gameState = ['null','null','null','null','null','null','null','null','null']
+let gameState = ['null','null','null','null','null','null','null','null','null']
 
 
 //click sound
@@ -16,9 +16,11 @@ const click = new Audio('/assets/click.mp3');
 squares.forEach((square) => square.addEventListener('click', clickSquare))
 
 //which box was clicked
-
-
 function clickSquare(event) {
+    if (gameOver){
+    return;
+    }
+
     const square = event.target
     
     if(square.innerText !=''){
@@ -35,33 +37,33 @@ function clickSquare(event) {
         currentPlayer = playerX
     }{
     click.play()
+    checkWinner()
     }
 }
-
-function checkWinner() {
-    //Check for a winner
-    for (const winningCombination of playerWins) {
-      //Object Destructuring
-      const { combo, strikeClass } = winningCombination;
-      const squareValue1 = gameState[combo[0] ];
-      const squareValue2 = gameState[combo[1] ];
-      const squareValue3 = gameState[combo[2] ];
-  
-      if ( tileValue1 != 'null' && squareValue1 === squareValue2 && squareValue1 === squareValue3
-      ){
-          gameOver=true
-          return
-      }
+//set comboos equal to values to tell the game when to stop if all squares are filled with same value
+function checkWinner(){
+    for(const winningCombination of winnerCombos){
+        const combo = winningCombination.combo
+        console.log(winningCombination)
+        let squareValue1 = gameState[combo[0]]
+        let squareValue2 = gameState[combo[1]]
+        let squareValue3 = gameState[combo[2]]
+        if(squareValue1 != null && squareValue1 === squareValue2 && squareValue1 === squareValue3 ){
+            
+            return;
+        }
     }
+    
 }
 
-const playerWins = [
-    { combo: [0, 1, 2],},
-    { combo: [3, 4, 5],},
-    { combo: [6, 7, 8],},
-    { combo: [0, 3, 6],},
-    { combo: [1, 4, 7],},
-    { combo: [2, 5, 8],},
-    { combo: [0, 4, 8],},
-    { combo: [2, 4,6],},
-  ]
+
+let winnerCombos = [
+    {combo: [0,1,2]},
+    {combo: [3,4,5]},
+    {combo: [6,7,8]},
+    {combo: [0,3,6]},
+    {combo: [1,4,7]},
+    {combo: [2,5,8]},
+    {combo: [0,4,8]},
+    {combo: [2,4,6]}
+]
